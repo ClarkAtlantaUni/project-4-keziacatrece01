af2fd77c158bf166233695a8c18982fddf225261
keziacatrece01 <kezia.cook@students.cau.edu>
2021-12-13 20:32:06 -0500

    Project 4 Final Updates

--- src/cis301/project4/server/webapp.py
+++ src/cis301/project4/server/webapp.py
@@ -1,11 +1,11 @@
 #TODO Add the missing routes or complete the logic of the existing routes
 
-import json
-import os
+
+
 import uuid
 from datetime import datetime
 from hashlib import md5
-from pathlib import Path
+
 
 from cis301.project4.phonecall import PhoneCall
 from cis301.project4.server.PhoneBillServer import PhoneBillServer
@@ -14,34 +14,35 @@ from flask_bootstrap import Bootstrap
 
 from cis301.project4.util import Util
 
-app = PhoneBillServer( __name__ )
+app = PhoneBillServer(__name__)
 
 
 def run(port=8000, file="database.txt"):
     app.set_port(port)
     app.set_file(file)
-    Bootstrap( app )
+    Bootstrap(app)
     app.start(True)
 
 
 @app.route('/home')
-@app.route( '/' )
+@app.route('/')
 def home():
     message = dict()
-    message["date"] =datetime.now().strftime( '%Y' )
-    message["file"]= app.get_file()
+    message["date"] = datetime.now().strftime('%Y')
+    message["file"] = app.get_file()
     message["text"] = "Welcome to Phone Bill Application!"
     if session.get('user'):
         message["username"] = session['user']
-    return render_template( "index.html",message=message );
+    return render_template("index.html", message=message);
+
 
 @app.route('/auth', methods=["POST", "GET"])
 def authenticate():
-    if request.method=="GET":
+    if request.method == "GET":
         message = dict()
         message["date"] = datetime.now().strftime('%Y')
         message["text"] = "Enter your credentials"
-        return render_template( "template_login.html", message=message )
+        return render_template("template_login.html", message=message)
     else:
         # TODO: authenticate the user
         data = request.json
@@ -49,7 +50,7 @@ def authenticate():
         if data is None:
             return '{"res":"400", "text":"invalid username or password."}'
 
-        if len(data)==0 or not data['password'] or not data['email']:
+        if len(data) == 0 or not data['password'] or not data['email']:
             return '{"res":"400", "text":"Please enter username and password."}'
         user['password'] = str(md5(data['password'].encode()).digest())
         user['email'] = data['email']
@@ -60,8 +61,8 @@ def authenticate():
             message["user"] = user[0]
             message["text"] = "Welcome " + user[0]
             session['user'] = user[0]
-            session['uid']= user[1]
-            if data.get( 'client' ):
+            session['uid'] = user[1]
+            if data.get('client'):
                 return '{"res":"200", "text":"authenticated user."}'
             else:
                 return redirect('/user/home')
@@ -71,13 +72,13 @@ def authenticate():
 
 @app.route('/register', methods=["POST", "GET"])
 def register():
-    #TODO: register the user
+    # TODO: register the user
 
     if request.method == "GET":
         message = dict()
         message["date"] = datetime.now().strftime('%Y')
         message["text"] = "Register as a new user"
-        return render_template( "template_register.html", message=message )
+        return render_template("template_register.html", message=message)
     elif request.method == "POST":
         data = request.json
         user = dict()
@@ -85,85 +86,106 @@ def register():
         user['name'] = data['name']
         user['password'] = str(md5(data['password'].encode()).digest())
         user['email'] = data['email']
-        message =""
+        message = ""
         if app.get_userdb().insert_user(user):
             message = '{"res":"200", "text":"Your account is active now"}'
         else:
             message = '{"res":"400", "text":"user name already exist"}'
         return message
     else:
-        return render_template( "invalid_page.html", message="" )
+        return render_template("invalid_page.html", message="")
+
 
 @app.route('/logout', methods=["GET"])
 def logout():
     session.clear()
     message = dict()
     message['user'] = "Test User"
-    message["date"] = datetime.now().strftime( '%Y' )
+    message["date"] = datetime.now().strftime('%Y')
     message["text"] = "Welcome"
     if request.method == "GET":
         # TODO: authenticate the user
         return redirect("/home")
 
+
 @app.route('/user/home')
 def user_home():
     if session['user'] and session['uid']:
         message = dict()
-        message["date"] = datetime.now().strftime( '%Y' )
+        message["date"] = datetime.now().strftime('%Y')
         message["username"] = session['user']
         message["text"] = "Welcome " + session['user']
         return render_template('user.html', message=message)
     else:
         redirect('/auth')
 
+
 @app.route('/user/search')
 def user_search():
     if session.get('user') and session.get('uid'):
         message = dict()
-        message["date"] = datetime.now().strftime( '%Y' )
+        message["date"] = datetime.now().strftime('%Y')
         message["username"] = session['user']
-        message["text"] = "Welcome " + session['user']+ ". The seach function will be available soon!"
+        message["text"] = "Welcome " + session['user'] + ". The seach function will be available soon!"
         return render_template('index.html', message=message)
     else:
         redirect('/auth')
 
-@app.route('/user/add', methods=["GET","POST"])
-def add_phonecall():
-        if request.method=='GET':
-            #TODO: load the form from the template
-            if session.get( 'user' ) and session.get( 'uid' ):
-                return  '{"res":"401", "text":"Not implemented"}'
-            else:
-                redirect( '/auth' )
-        #gather phone call info.
-        elif request.method == 'POST':
-            if session.get( 'user' ) and session.get( 'uid' ):
-                #contains phonecall info
-                data = request.json
-                caller = data['caller']
-                callee = data['callee']
-                startdate = data['startdate']
-                enddate= data['enddate']
-                # check and validate user data
-                message=""
-                if Util.isValidPhoneNumber(caller) and Util.isValidPhoneNumber(callee):
-                    phonecall = PhoneCall( callee, caller, startdate, enddate )
-                    phonecall.set_uid(session.get('uid'))
-                    app.get_userdb().insert_phonecall(phonecall)
 
-                    #add the new phone call to the database
-                    print(phonecall)
-                    #if insertion is a success
-                    message = '{"res":"200", "text":"added a new phone call"}'
+@app.route('/user/del', methods=["POST"])
+def del_phonecall(phonecall_id=None):
+    if request.method == 'GET':
+        return '{"res":"401", "text":"Unauthorized client error"}', 401
+    if not (session.get('user') and session.get('uid')):
+        return '{"res":"401", "text":"Unauthorized access"}', 401
 
-                else:
-                    message = '{"res":"400", "text":"invalid phone call attributes"}'
-
-                return message
-            else:
-                return '{"res":"401", "text":"Auth failed"}'
+    data = request.json
+    phonecall_id = data['phonecall_id']
+    if phonecall_id:
+        if app.get_userdb().delete_phonecall(phonecall_id,session.get('uid')):
+            return '{"res":"200", "text":"Deleted"}', 200
         else:
-            return '{"res":"401", "text":"Method not supported"}'
+            return '{"res":"401", "text":"Could not delete {phonecall_id}"}', 200
+
+
+@app.route('/user/add', methods=["GET", "POST"])
+def add_phonecall():
+    if request.method == 'GET':
+        # TODO: load the form from the template
+        if session.get('user') and session.get('uid'):
+            return '{"res":"401", "text":"Not implemented"}'
+        else:
+            redirect('/auth')
+    # gather phone call info.
+    elif request.method == 'POST':
+        if session.get('user') and session.get('uid'):
+            # contains phonecall info
+            data = request.json
+            caller = data['caller']
+            callee = data['callee']
+            startdate = data['startdate']
+            enddate = data['enddate']
+            # check and validate user data
+            message = ""
+            if Util.isValidPhoneNumber(caller) and Util.isValidPhoneNumber(callee):
+                phonecall = PhoneCall(callee, caller, startdate, enddate)
+                phonecall.set_uid(session.get('uid'))
+                app.get_userdb().insert_phonecall(phonecall)
+
+                # add the new phone call to the database
+                print(phonecall)
+                # if insertion is a success
+                message = '{"res":"200", "text":"added a new phone call"}'
+
+            else:
+                message = '{"res":"400", "text":"invalid phone call attributes"}'
+
+            return message
+        else:
+            return '{"res":"401", "text":"Auth failed"}'
+    else:
+        return '{"res":"401", "text":"Method not supported"}'
+
 
 if __name__ == "__main__":
     run()
